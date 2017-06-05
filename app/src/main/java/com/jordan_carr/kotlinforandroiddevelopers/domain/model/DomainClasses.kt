e3c7bc16c0250eaefb51796fa91351c0995ec626
Jordan Carr <jordancarr42@gmail.com>
2017-06-05 17:07:49 -0600

    Chapter 11

--- app/src/main/java/com/jordan_carr/kotlinforandroiddevelopers/domain/model/DomainClasses.kt
+++ app/src/main/java/com/jordan_carr/kotlinforandroiddevelopers/domain/model/DomainClasses.kt
@@ -4,6 +4,9 @@ package com.jordan_carr.kotlinforandroiddevelopers.domain.model
  * Created by Jordan Carr on 2017-06-05.
  */
 
-data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>)
+data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>) {
+    operator fun get(position: Int): Forecast = dailyForecast[position]
+    fun size(): Int = dailyForecast.size
+}
 
 data class Forecast(val date: String, val description: String, val high: Int, val low: Int)
--- app/src/main/java/com/jordan_carr/kotlinforandroiddevelopers/ui/adapters/ForecastListAdapter.kt
+++ app/src/main/java/com/jordan_carr/kotlinforandroiddevelopers/ui/adapters/ForecastListAdapter.kt
@@ -9,19 +9,20 @@ import com.jordan_carr.kotlinforandroiddevelopers.domain.model.ForecastList
  * Created by Jordan Carr on 2017-06-04.
  */
 
-class ForecastListAdapter(val weekForecast: ForecastList) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+class ForecastListAdapter(
+        val weekForecast: ForecastList) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder? {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(TextView(parent.context))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        with(weekForecast.dailyForecast[position]) {
+        with(weekForecast[position]) {
             holder.textView.text = "$date - $description - $high/$low"
         }
     }
 
-    override fun getItemCount(): Int = weekForecast.dailyForecast.size
+    override fun getItemCount(): Int = weekForecast.size()
 
     class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 }
