4a671de3824eb4ccbe260313191b39a2b2c5be13
Jordan Carr <jordancarr42@gmail.com>
2017-06-05 20:46:58 -0600

    Updated reference to renamed package

--- app/src/androidTest/java/com/jordan_carr/KWeather/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/jordan_carr/KWeather/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.jordan_carr.kotlinforandroiddevelopers", appContext.getPackageName());
+        assertEquals("com.jordan_carr.KWeather", appContext.getPackageName());
     }
 }
