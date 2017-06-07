22720f80c350c2f1ff1e8e5094e26bf2395830a2
Jordan Carr <jordancarr42@gmail.com>
2017-06-06 20:02:48 -0600

    Chapter 14

--- app/src/main/java/com/jordan_carr/KWeather/domain/model/DomainClasses.kt
+++ app/src/main/java/com/jordan_carr/KWeather/domain/model/DomainClasses.kt
@@ -5,8 +5,11 @@ package com.jordan_carr.KWeather.domain.model
  */
 
 data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>) {
-    operator fun get(position: Int): Forecast = dailyForecast[position]
-    fun size(): Int = dailyForecast.size
+    val size: Int
+        get() = dailyForecast.size
+
+    operator fun get(position: Int) = dailyForecast[position]
 }
 
-data class Forecast(val date: String, val description: String, val high: Int, val low: Int, val iconUrl: String)
+data class Forecast(val date: String, val description: String, val high: Int, val low: Int,
+                    val iconUrl: String)
